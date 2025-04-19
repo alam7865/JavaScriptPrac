6c367181c53559e0c8796fd15b26ddfbe83ea83d
alam7865 <sabbajalam5@gmail.com>
2025-04-19 23:01:54 +0530

    prac

--- Practise/async1.js
+++ Practise/async1.js
@@ -18,20 +18,40 @@
 //   });
 
 ////////////////////////////////////////////
-const p = new Promise((resolve, reject) => {
-  resolve("Resolved Promise Data");
-});
-
-// console.log(p);
-// p.then((x) => {
-//   console.log(x);
+// const p = new Promise((resolve, reject) => {
+//   resolve("Resolved Promise Data");
 // });
 
-function getData() {
-  return p;
+// // console.log(p);
+// // p.then((x) => {
+// //   console.log(x);
+// // });
+
+// function getData() {
+//   return p;
+// }
+
+// const data = getData();
+// data.then((x) => {
+//   console.log(x);       // Resolved Promise Data
+// });
+
+////////////////////////////////////////////////////////
+const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Hello Alam Bhai");
+  }, 3000);
+});
+
+async function getData() {
+  return await p;
 }
 
 const data = getData();
-data.then((x) => {
-  console.log(x);
-});
+data
+  .then((x) => {
+    console.log(x);
+  })
+  .catch((err) => {
+    console.log("Error: " + err.mesage);
+  });
